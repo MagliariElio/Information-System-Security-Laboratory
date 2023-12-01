5f6750bd5297a9be95b23dbf36e4ffbaadbfa96e
MagliariElio <magliari.elio08@gmail.com>
2023-12-01 19:28:56 +0100

    fix: update

--- LAB_4/performance/run_policy_state.sh
+++ LAB_4/performance/run_policy_state.sh
@@ -1,12 +1,12 @@
 
-echo -e "flush of the states"
+echo "flush of the states"
 ip xfrm state flush
 
-echo -e "flush of the policies"
+echo "flush of the policies"
 ip xfrm policy flush
 
 
-echo -e "ESP with AES-128-CBC"
+echo "ESP with AES-128-CBC"
 
 ip xfrm state add src 192.168.82.97 dst 192.168.82.78 proto esp spi 0x1000 enc aes 0xaa223344556677889900aabbccddeeff
 ip xfrm state add src 192.168.82.78 dst 192.168.82.97 proto esp spi 0x2000 enc aes 0xbb223344556677889900aabbccddeeff
@@ -14,7 +14,7 @@ ip xfrm policy add src 192.168.82.97 dst 192.168.82.78 dir out tmpl proto esp mo
 ip xfrm policy add src 192.168.82.78 dst 192.168.82.97 dir in tmpl proto esp mode transport level required
 
 
-# echo -e "ESP with AES-128-CBC and HMAC-SHA1"
+# echo "ESP with AES-128-CBC and HMAC-SHA1"
 
 # ip xfrm state add src 192.168.82.97 dst 192.168.82.78 proto esp spi 0x1000 enc aes 0xaa223344556677889900aabbccddeeff auth sha1 0xbbccddeeff00112233445566778899aabbccddeeff
 # ip xfrm state add src 192.168.82.78 dst 192.168.82.97 proto esp spi 0x2000 enc aes 0xbb223344556677889900aabbccddeeff auth sha1 0xaaccddeeff00112233445566778899aabbccddeeff
@@ -22,7 +22,7 @@ ip xfrm policy add src 192.168.82.78 dst 192.168.82.97 dir in tmpl proto esp mod
 # ip xfrm policy add src 192.168.82.78 dst 192.168.82.97 dir in tmpl proto esp mode transport level required
 
 
-# echo -e "AH with HMAC-SHA1"
+# echo "AH with HMAC-SHA1"
 
 # ip xfrm state add src 192.168.82.97 dst 192.168.82.78 proto ah spi 0x3000 auth sha1 0xbbccddeeff00112233445566778899aabbccddeeff
 # ip xfrm state add src 192.168.82.78 dst 192.168.82.97 proto ah spi 0x4000 auth sha1 0xaaccddeeff00112233445566778899aabbccddeeff
@@ -30,11 +30,11 @@ ip xfrm policy add src 192.168.82.78 dst 192.168.82.97 dir in tmpl proto esp mod
 # ip xfrm policy add src 192.168.82.78 dst 192.168.82.97 dir in tmpl proto ah mode transport level required
 
 
-echo -e "\nstates:"
+echo "\nstates:"
 ip xfrm state
 
-echo -e "\npolicies:"
+echo "\npolicies:"
 ip xfrm policy
 
-echo -e "\n restart of ipsec service"
+echo "\nrestart of ipsec service"
 ipsec restart
