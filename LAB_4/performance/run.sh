4e05cbf0d675ee37b2717da2572920c04ce67359
MagliariElio <magliari.elio08@gmail.com>
2023-12-01 18:38:56 +0100

    fix: update

---
+++ LAB_4/performance/run.sh
@@ -0,0 +1,19 @@
+echo -e "Download file size: 10 kb"
+
+curl --trace-time -v -o /dev/null http://192.168.82.78/ipsec/file_10kb
+
+echo -e "\n\nDownload file size: 100 kb"
+
+curl --trace-time -v -o /dev/null http://192.168.82.78/ipsec/file_100kb
+
+echo -e "\n\nDownload file size: 1 mb"
+
+curl --trace-time -v -o /dev/null http://192.168.82.78/ipsec/file_1mb
+
+echo -e "\n\nDownload file size: 10 mb"
+
+curl --trace-time -v -o /dev/null http://192.168.82.78/ipsec/file_10mb
+
+echo -e "\n\nDownload file size: 100 mb"
+
+curl --trace-time -v -o /dev/null http://192.168.82.78/ipsec/file_100mb
