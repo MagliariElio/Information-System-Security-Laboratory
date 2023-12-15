fd8bea5d8b4aa82ec053d67dda335f832c227c17
elion-man <magliari.elio08@gmail.com>
2023-12-15 18:00:41 +0100

    last lab 5 update

---
+++ LAB_5/icmp.sh
@@ -0,0 +1,22 @@
+
+iptables -F INPUT
+iptables -F FORWARD
+iptables -F OUTPUT
+
+iptables -P FORWARD DROP
+
+iptables -A FORWARD -p tcp -s 192.168.188.158 --dport 80 -j ACCEPT
+iptables -A FORWARD -p tcp -d 192.168.188.158 --sport 80 -j ACCEPT
+iptables -I FORWARD 2 -p tcp -d 192.168.188.158 --sport 80 --syn -j DROP
+
+#public service
+iptables -A FORWARD -p tcp -d 192.168.188.158 --dport 22 -j ACCEPT
+iptables -A FORWARD -p tcp -s 192.168.188.158 --sport 22 --syn -j DROP
+iptables -A FORWARD -p tcp -s 192.168.188.158 --sport 22 -j ACCEPT
+
+#icmp
+iptables -A FORWARD -p icmp -s 192.168.188.158 --icmp-type echo-request -j ACCEPT
+iptables -A FORWARD -p icmp -d 192.168.188.158 --icmp-type echo-reply -j ACCEPT
+iptables -A FORWARD -p icmp -d 192.168.188.158 --icmp-type echo-request -m limit --limit 1/minute --limit-burst 0 -j ACCEPT
+iptables -A FORWARD -p icmp -s 192.168.188.158 --icmp-type echo-reply -j ACCEPT
+
---
+++ LAB_5/statefull_packet_filter.sh
@@ -0,0 +1,30 @@
+
+iptables -F
+
+iptables -P FORWARD DROP
+
+iptables -A FORWARD -p tcp -s 192.168.188.158 --dport 80 -j ACCEPT
+iptables -A FORWARD -m state  --state ESTABLISHED,RELATED -j ACCEPT
+
+#iptables -A FORWARD -p tcp -s 192.168.188.97 --dport 22 -j ACCEPT
+
+
+#iptables -A FORWARD -p tcp -s 192.168.188.158 --dport 80 -j ACCEPT
+#iptables -A FORWARD -p tcp -d 192.168.188.158 --sport 80 -j ACCEPT
+#iptables -I FORWARD 2 -p tcp -d 192.168.188.158 --sport 80 --syn -j DROP
+
+#public service
+#iptables -A FORWARD -p tcp -d 192.168.188.158 --dport 22 -j ACCEPT
+#iptables -A FORWARD -p tcp -s 192.168.188.158 --sport 22 --syn -j DROP
+#iptables -A FORWARD -p tcp -s 192.168.188.158 --sport 22 -j ACCEPT
+
+#icmp
+#iptables -A FORWARD -p icmp -s 192.168.188.158 --icmp-type echo-request -j ACCEPT
+#iptables -A FORWARD -p icmp -d 192.168.188.158 --icmp-type echo-reply -j ACCEPT
+#iptables -A FORWARD -p icmp -d 192.168.188.158 --icmp-type echo-request -j ACCEPT
+#iptables -A FORWARD -p icmp -s 192.168.188.158 --icmp-type echo-reply -j ACCEPT
+
+#bandwith limitation
+#iptables -D FORWARD 1
+#iptables -A FORWARD -p icmp -s 192.168.188.158 --icmp-type echo-request -m limit --limit 10/minute --limit-burst 10 -j ACCEPT
+#iptables -A FORWARD -p icmp -d 192.168.188.158 --icmp-type echo-reply -j ACCEPT
